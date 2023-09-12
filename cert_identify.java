903f0dfeafb4f31ec5b23473a7253e8624b93223
root <root@ip-172-31-47-106.ec2.internal>
2023-09-12 03:50:45 +0000

    This is the second commit

---
+++ cert_identify.java
@@ -0,0 +1,40 @@
+const https = require('https');
+
+const hostname = 'example.com'; // Replace with the hostname of the server you want to check
+const port = 443; // Default HTTPS port
+
+// Create an HTTPS request to the server
+const options = {
+  hostname,
+  port,
+  method: 'GET',
+};
+
+const req = https.request(options, (res) => {
+  const certificate = res.socket.getPeerCertificate();
+
+  if (!certificate.valid_to) {
+    console.log('Unable to retrieve certificate expiration date.');
+  } else {
+    const expirationDate = new Date(certificate.valid_to);
+    const currentDate = new Date();
+
+    const daysUntilExpiration = Math.floor(
+      (expirationDate - currentDate) / (1000 * 60 * 60 * 24)
+    );
+
+    console.log('SSL Certificate Expiration Date:', expirationDate.toISOString());
+    console.log('Days Until Expiration:', daysUntilExpiration);
+  }
+
+  // Close the HTTPS request
+  req.end();
+});
+
+req.on('error', (error) => {
+  console.error('Error:', error);
+});
+
+// Finish the request
+req.end();
+
